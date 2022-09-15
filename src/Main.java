e29d877ad3e1b644899dc1f231c8c4649dac7df5
antonyare93 <antonyare_93@hotmail.com>
2022-09-15 10:09:13 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println(suma(10,20,30));
+        coche miCoche = new coche();
+        miCoche.agregar_puerta();
+        System.out.println(miCoche.puertas);
+    }
+
+    public static int suma(int a, int b, int c){
+        return a+b+c;
+    }
+}
+
+class coche{
+    int puertas = 0;
+
+    public void agregar_puerta(){
+        this.puertas++;
+    }
+}
